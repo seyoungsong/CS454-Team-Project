a73aecdb8d7985ba970ca0b34607cad92d399155
seyoung <seyoung@macos>
2021-11-16 20:51:55 +0900

    json

--- temp.py
+++ temp.py
@@ -1,4 +1,19 @@
+import json
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
 
-tree = ET.parse("country_data.xml")
-root = tree.getroot()
+
+def pp(root: Element):
+    for child in root:
+        print(child.tag, child.attrib)
+
+
+tree = ET.parse("clover.xml")
+coverage = tree.getroot()
+testproject = coverage[1]
+package = testproject[1]
+files = [e for e in package if e.tag == "file"]
+data = {f.attrib["name"]: [e.attrib for e in f if e.tag == "line"] for f in files}
+
+with open("clover.json", "w") as f:
+    json.dump(data, f, indent=4, sort_keys=True)

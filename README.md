13495097488bf5319c0c2fe0af2a945b2dad74c7
seyoung <seyoung@macos>
2021-11-16 23:44:48 +0900

    a

--- README.md
+++ README.md
@@ -97,7 +97,7 @@ cp -R target/site/clover ./clover
 
 # open report in web browser (you can check the time duration of each test)
 open target/site/clover/index.html
-open clover/index.html
+open clover/index.html  # cd ~/java-uuid-generator
 code clover/clover.xml
 
 # run clover for 1 unit test
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import glob
 import json
 import xml.etree.ElementTree as ET
+from pprint import pprint
 from xml.etree.ElementTree import Element
 
 
@@ -9,8 +10,9 @@ def flatten(t):
 
 
 def pp(root: Element):
-    for child in root:
-        print(child.tag, child.attrib)
+    for e in root:
+        d = [e.tag, e.attrib]
+        pprint(d)
 
 
 data_dir = "clover_by_test"
@@ -21,25 +23,25 @@ xml_file = filenames[0]
 
 
 tree = ET.parse(xml_file)
-coverage = tree.getroot()
-testproject = coverage[1]
-package = testproject[1]
-files = [e for e in package if e.tag == "file"]
-signatures = flatten(
-    [
-        [
-            (f.attrib["name"].split(".java")[0], d["signature"].split("() : ")[0])
-            for d in [e.attrib for e in f if e.tag == "line"]
-            if "testsuccess" in d
-        ]
-        for f in files
-    ]
-)
+root = tree.getroot()
+project = root[0]
+output = []
+for package in project:
+    files = [e for e in package if e.tag == "file"]
+    for file in files:
+        java_name = file.attrib["name"]
+        lines = [e.attrib for e in file if e.tag == "line"]
+        covered = []
+        for line in lines:
+            num = line["num"]
+            if "count" in line and int(line["count"]) > 0:
+                covered.append(num)
+            if "truecount" in line and int(line["truecount"]) > 0:
+                covered.append(f"{num}T")
+            if "falsecount" in line and int(line["falsecount"]) > 0:
+                covered.append(f"{num}F")
+        if covered:
+            output.append({java_name: covered})
 
-cmds = [
-    f"mvn clean clover:setup -Dtest={test_class}#{method} test clover:aggregate clover:clover && cp target/site/clover/clover.xml ./clover_by_test/clover_{test_class}_{method}.xml"
-    for test_class, method in signatures
-]
-
-with open("cmds.txt", "w") as f:
-    f.write("\n".join(cmds))
+pprint(output)
+covered = output[0]["UUIDUtil.java"]

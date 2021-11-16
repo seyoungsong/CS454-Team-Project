6a6ba2b8fab304599d96900539b9fc0ff553e62a
seyoung <seyoung@macos>
2021-11-16 23:55:00 +0900

    a

--- test.py
+++ test.py
@@ -16,32 +16,29 @@ def pp(root: Element):
 
 
 data_dir = "clover_by_test"
-
-filenames = glob.glob(f"{data_dir}/*.xml")
-
-xml_file = filenames[0]
+xml_files = glob.glob(f"{data_dir}/*.xml")
+xml_files.sort()
+xml_file = xml_files[0]
 
 
 tree = ET.parse(xml_file)
 root = tree.getroot()
 project = root[0]
-output = []
+code_coverage = {}
 for package in project:
     files = [e for e in package if e.tag == "file"]
     for file in files:
-        java_name = file.attrib["name"]
+        filename = file.attrib["name"]
         lines = [e.attrib for e in file if e.tag == "line"]
-        covered = []
+        nums = []
         for line in lines:
             num = line["num"]
             if "count" in line and int(line["count"]) > 0:
-                covered.append(num)
+                nums.append(num)
             if "truecount" in line and int(line["truecount"]) > 0:
-                covered.append(f"{num}T")
+                nums.append(f"{num}T")
             if "falsecount" in line and int(line["falsecount"]) > 0:
-                covered.append(f"{num}F")
-        if covered:
-            output.append({java_name: covered})
-
-pprint(output)
-covered = output[0]["UUIDUtil.java"]
+                nums.append(f"{num}F")
+        if nums:
+            nums.sort()
+            code_coverage[filename] = nums

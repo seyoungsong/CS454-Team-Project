d8a2f05abcdba660017fe680e1fc10c954ff8979
subeom272 <subeom272@kaist.ac.kr>
2021-12-18 05:08:27 +0900

    new performance analysis

--- subeom/analysis.py
+++ subeom/analysis.py
@@ -1,19 +1,24 @@
 import csv
 import json
 
-with open("result4.json") as resultfile:
+with open("result_SA.json") as resultfile:
     resultdata = json.load(resultfile)
 
 performances = [
     [1],
     [1] * 2,
     [1] * 4,
-    [1] * 8,
-    [1] * 16,
-    [1, 2],
     [1, 3],
-    [1, 4],
-    [1, 1, 1, 1, 4, 4, 4, 4],
+    [1, 1, 2],
+    [2, 2],
+    [1] * 8,
+    [1, 7],
+    [2, 6],
+    [3, 5],
+    [4, 4],
+    [2, 2, 2, 2],
+    [1, 1, 3, 3],
+    [1] * 100,
 ]
 
 data = [
@@ -21,12 +26,17 @@ data = [
     [[1]],
     [[1] * 2],
     [[1] * 4],
-    [[1] * 8],
-    [[1] * 16],
-    [[1, 2]],
     [[1, 3]],
-    [[1, 4]],
-    [[1, 1, 1, 1, 4, 4, 4, 4]],
+    [[1, 1, 2]],
+    [[2, 2]],
+    [[1] * 8],
+    [[1, 7]],
+    [[2, 6]],
+    [[3, 5]],
+    [[4, 4]],
+    [[2, 2, 2, 2]],
+    [[1, 1, 3, 3]],
+    [[1] * 100],
 ]
 
 for datafile in resultdata:
@@ -36,7 +46,7 @@ for datafile in resultdata:
         data[i].append(result["APSC_c"])
         i = i + 1
 
-f = open("result4.csv", "w", newline="")
+f = open("result_SA.csv", "w", newline="")
 writer = csv.writer(f)
 writer.writerows(data)
 f.close()
